2144c76ef77d708f23845eb47c35d6d75ebf5af3
Arnbiorn <egor.skrutelev@gmail.com>
2021-02-22 19:45:11 +0300

    Added settings for Contact Details View Controller

--- Contacts App/ViewControllers/ContactDetailsViewController.swift
+++ Contacts App/ViewControllers/ContactDetailsViewController.swift
@@ -5,8 +5,14 @@ class ContactDetailsViewController: UIViewController {
     @IBOutlet var phoneNumberLabel: UILabel!
     @IBOutlet var emailLabel: UILabel!
     
+    var person: Person!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = person.fullName
+        phoneNumberLabel.text = "Phone: \(person.phoneNumber)"
+        emailLabel.text = "Email: \(person.email)"
     }
 }
